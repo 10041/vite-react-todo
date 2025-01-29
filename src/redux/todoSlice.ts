c060d9bf0aa52307cae4f4ff0bea85d196c252ce
Oleksyuk <a.oleksyuk@itransition.com>
2025-01-29 18:43:25 +0100

    Add redux and slice sample

---
+++ src/redux/todoSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice } from '@reduxjs/toolkit'
+import type { PayloadAction } from '@reduxjs/toolkit'
+
+export interface TodoState {
+  value: number
+}
+
+const initialState: TodoState = {
+  value: 0,
+}
+
+export const counterSlice = createSlice({
+  name: 'todo',
+  initialState,
+  reducers: {
+    increment: (state) => {
+      state.value += 1
+    },
+    decrement: (state) => {
+      state.value -= 1
+    },
+    incrementByAmount: (state, action: PayloadAction<number>) => {
+      state.value += action.payload
+    },
+  },
+})
+
+// Action creators are generated for each case reducer function
+export const {
+  increment,
+  decrement,
+  incrementByAmount
+} = counterSlice.actions
+
+export default counterSlice.reducer
